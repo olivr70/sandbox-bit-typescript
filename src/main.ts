8f20a49fc5f2c8d82f1203c351ed8313ed24b78d
Olivier Chevet <olivr70@outlook.fr>
2019-06-15 16:31:10 +0200

    exported and ejected sum

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { sum } from "./components/Sum";
-// import { sum } from "@bit.olivr70.sandbox.sum";
+//import { sum } from "./components/Sum";
+import { sum } from "@bit/olivr70.sandbox.sum";
 
 console.log("should be 6. Got ", sum(1, 2, 3));
